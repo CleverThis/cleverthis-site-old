7c1004b6958a74ec3273825d4a0e417373de72c6
Jeffrey Phillips Freeman <the@jeffreyfreeman.me>
2020-03-01 19:02:00 +0100

    Cleaned up some bits, minor.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,35 +10,6 @@ import { Controller, Scene } from 'react-scrollmagic'
 import { Tween, Timeline } from 'react-gsap'
 import styled from 'styled-components'
 
-const style = {
-  emphesis: css`
-    color: yellow;
-  `
-}
-
-const TweenStyled = styled.div`
-  .section {
-    height: 100vh;
-  }
-
-  .tween {
-    width: 100px;
-    height: 100px;
-    background-color: red;
-    margin: 0 !important;
-    position: relative;
-    left: calc(50% - 50px);
-  }
-
-  .stagger {
-    width: 50px;
-    height: 50px;
-    left: 700px;
-    background-color: green;
-    position: relative;
-  }
-`
-
 export const IndexPage = () => (
   <>
     <div className="stars"></div>
@@ -57,58 +28,47 @@ export const IndexPage = () => (
         </ul>
       </div>
     </div>
-    <div>
-      <TweenStyled>
-        <div id="trigger-comet" />
-        <Controller>
-          <Scene triggerElement="#trigger-comet" duration={1200} pin={false}>
-            {progress => (
-              <Tween
-                from={{
-                  css: {
-                    left: '-150px',
-                    top: '-35vh'
-                  }
-                }}
-                to={{
-                  css: {
-                    left: '120vw',
-                    top: '30vh'
-                  }
-                }}
-                totalProgress={progress}
-                paused
-              >
-                <div className="comet"></div>
-              </Tween>
-            )}
-          </Scene>
-        </Controller>
-      </TweenStyled>
-      <div className="nebula" />
-      <div className="textcard textcard-cloud">
-        <div className="textcard-content">
-          <h2>The Cloud</h2>
-          <p>
-            The cloud is a place of fluffiness with things of fluff and rain and rainbows.
-            Fluffiness is how soft something is, rocks arent fluffy, clouds are. So buy our product
-            and be fluffy!
-          </p>
+    <div id="trigger-comet" />
+    <Controller>
+      <Scene triggerElement="#trigger-comet" duration={1200} pin={false}>
+        {progress => (
+          <Tween
+            from={{
+              css: {
+                left: '-150px',
+                top: '-35vh'
+              }
+            }}
+            to={{
+              css: {
+                left: '120vw',
+                top: '30vh'
+              }
+            }}
+            totalProgress={progress}
+            paused
+          >
+            <div className="comet"></div>
+          </Tween>
+        )}
+      </Scene>
+      <Scene duration={4800} offset={250} pin>
+        <div className="cloud-section">
+          <div className="nebula" />
+          <div className="textcard textcard-cloud">
+            <div className="textcard-content">
+              <h2>The Cloud</h2>
+              <p>
+                The cloud is a place of fluffiness with things of fluff and rain and rainbows.
+                Fluffiness is how soft something is, rocks arent fluffy, clouds are. So buy our
+                product and be fluffy!
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div id="animation-end"></div>
-    </div>
-    {/* <div>
-        <h2>Hi people</h2>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <p>
-        <Link to="/another-page/">Go to another page</Link>
-        </p>
-        <p>
-        <Link to="/all/">See content generated from Markdown files</Link>
-        </p>
-        </div> */}
+      </Scene>
+    </Controller>
+    <div id="animation-end"></div>
   </>
 )
 
